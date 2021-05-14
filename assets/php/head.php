685e8669bd1117274fdddbf0c5e97f9daac7a85e
GArysTrousers <43531407+GArysTrousers@users.noreply.github.com>
2021-05-14 11:51:36 +1000

    Update head.php

--- assets/php/head.php
+++ assets/php/head.php
@@ -1,3 +1,4 @@
+<?php ?>
 <link rel="apple-touch-icon" sizes="180x180" href="/assets/icon/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/assets/icon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/assets/icon/favicon-16x16.png">
